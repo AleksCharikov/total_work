0d8f39fe2ac27bc4662cf84bd0e713ff5e691d10
AleksCha <charikov91@gmail.com>
2023-01-29 19:18:27 +0300

    add decisiom

--- Program.cs
+++ Program.cs
@@ -9,3 +9,47 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+Console.WriteLine("Введите длину массива:");
+int n = int.Parse(Console.ReadLine());
+string[] array = new string[n];
+array = FillArrayString(array);
+Console.WriteLine("Введенный массив:");
+PrintArray(array);
+string[] threeChar = StringThreeCharFromArray(array);
+Console.WriteLine("Массив из строк, длина которых либо меньше либо равна 3 символа:");
+PrintArray(threeChar);
+
+string[] FillArrayString(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите элемент № {i + 1}");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] StringThreeCharFromArray(string[] array)
+{
+    string[] result = new string[array.Length];
+    int length = 3;
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= length)
+        {
+            result[count] = array[i];
+            count++;
+        }
+    }
+    return result;
+}
